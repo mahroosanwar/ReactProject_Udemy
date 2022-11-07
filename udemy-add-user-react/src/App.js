33e6f748678ca8f59d31c4a5158b8124095d5ddf
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-11-08 00:29:32 +0530

    Add files via upload

---
+++ udemy-add-user-react/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, {useState} from 'react';
+import AddUser from './components/Users/AddUsers';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, {name: uName, age: uAge, id: Math.random().toString()}];
+    });
+  };
+
+  return (
+    <>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </>
+  );
+}
+
+export default App;
