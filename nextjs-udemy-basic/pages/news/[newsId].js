0ea76f4867883e044898ccba4de557296ffde93c
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-11-08 00:03:37 +0530

    Add files via upload

---
+++ nextjs-udemy-basic/pages/news/[newsId].js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+
+//our-domain.com/news/something-important
+
+function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+    console.log(newsId);
+  return <h1>The Detail Page</h1>;
+}
+
+export default DetailPage;
