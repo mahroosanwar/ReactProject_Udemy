8b622c8eeea7708973ba2e777cf46cc1709a42a6
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-11-07 23:57:12 +0530

    Add files via upload

---
+++ food-order-app-react-project/src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Card.module.css';
+
+const Card = props => {
+
+};
+
+export default Card;
---
+++ food-order-app-react-project/src/components/UI/Modal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Modal.module.css';
+
+const Modal = props => {
+
+};
+
+export default Modal;
